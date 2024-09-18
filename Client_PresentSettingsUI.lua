c46c859b80a7c162774b08ca931ab9c8351bd935
Tracy Treadway <treadmen@hotmail.com>
2024-09-18 13:03:40 -0400

    one more tweak to the neutral message

--- Client_PresentSettingsUI.lua
+++ Client_PresentSettingsUI.lua
@@ -1,8 +1,6 @@
 function Client_PresentSettingsUI(rootParent)
 	local vert = UI.CreateVerticalLayoutGroup(rootParent);
-	if Mod.Settings.goNeutral ~= nil then
-		if Mod.Settings.goNeutral == true then
-			UI.CreateLabel(vert).SetText('The empty territory turns neutral');
-		end
+	if Mod.Settings.goNeutral ~= nil and Mod.Settings.goNeutral == true then
+		UI.CreateLabel(vert).SetText('The empty territory turns neutral');
 	end
 end
